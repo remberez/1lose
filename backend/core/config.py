a7628627ea09c32506a4f6f4a07e06b4484c4621
Agasyan <agasyan222@gmail.com>
2025-04-01 17:35:14 +0400

    create a database config

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunSettings(BaseModel):
@@ -12,9 +12,25 @@ class APISettings(BaseModel):
     prefix: str = "/api"
 
 
+class DataBaseSettings(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP__",
+        env_file=".env"
+    )
+
     run: RunSettings = RunSettings()
     api: APISettings = APISettings()
+    database: DataBaseSettings
 
 
 settings = Settings()
