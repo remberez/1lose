1a65e83f8b6424d3665496c6e103b73b4b8defa1
Agasyan <agasyan222@gmail.com>
2025-04-01 17:51:24 +0400

    create access_token model

---
+++ backend/core/models/access_token.py
@@ -0,0 +1,22 @@
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyBaseAccessTokenTable,
+    SQLAlchemyAccessTokenDatabase,
+)
+from sqlalchemy import Integer, ForeignKey
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+from core.types.user_id import UserID
+
+
+class AccessTokenModel(Base, SQLAlchemyBaseAccessTokenTable[UserID]):
+    user_id: Mapped[UserID] = mapped_column(
+        Integer,
+        ForeignKey("user.id", ondelete="cascade"),
+        nullable=True,
+    )
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyAccessTokenDatabase(session, cls)
