407c121c7a294dc44ab0eb79f1e82d7ede0ca71c
Agasyan <agasyan222@gmail.com>
2025-04-01 13:54:49 +0400

    create base router

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from api import router
+import uvicorn
+from core.config import settings
+
+app = FastAPI()
+app.include_router(router=router)
+
+if __name__ == '__main__':
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=settings.run.reload,
+    )
