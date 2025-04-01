ad738ea2be488ca2130d216bc4574ecf79debde1
Agasyan <agasyan222@gmail.com>
2025-04-01 17:34:37 +0400

    add integer id mixin

--- backend/core/models/base.py
+++ backend/core/models/base.py
@@ -10,6 +10,10 @@ class Base(DeclarativeBase):
     pass
 
 
+class IntegerIDMixin:
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
 class DateCreatedUpdatedMixin:
     created_at: Mapped[datetime] = mapped_column(default=datetime.now, nullable=True)
     updated_at: Mapped[datetime] = mapped_column(default=datetime.now, onupdate=datetime.now, nullable=True)
