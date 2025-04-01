fe38b36910c7a0f6fbffa04ee6253252195a87c8
Agasyan <agasyan222@gmail.com>
2025-04-01 13:54:38 +0400

    create base project settings

---
+++ backend/core/config.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class RunSettings(BaseModel):
+    port: int = 8000
+    host: str = "localhost"
+    reload: bool = True
+
+
+class APISettings(BaseModel):
+    prefix: str = "/api"
+
+
+class Settings(BaseSettings):
+    run: RunSettings = RunSettings()
+    api: APISettings = APISettings()
+
+
+settings = Settings()
