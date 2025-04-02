96b2b6edc59e342db6f064db4213b3467933ef24
Agasyan <agasyan222@gmail.com>
2025-04-02 13:10:55 +0400

    create a game model

---
+++ backend/core/models/game.py
@@ -0,0 +1,10 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base, IntegerIDMixin
+
+class GameModel(Base, IntegerIDMixin):
+    __tablename__ = "game"
+
+    name: Mapped[str] = mapped_column(String(25))
+    description: Mapped[str]
