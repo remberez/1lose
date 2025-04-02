95f98806b2334a466045e5a024ea1607a6ce4506
Agasyan <agasyan222@gmail.com>
2025-04-02 13:24:58 +0400

    create abstract sqlalchemy repository

---
+++ backend/core/repository/sqlalchemy.py
@@ -0,0 +1,13 @@
+from abc import ABC
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from .abc import AbstractRepository
+
+
+class SQLAlchemyAbstractRepository[Model](AbstractRepository[Model], ABC):
+    """
+    Интерфейс репозитория SQLAlchemy, от которого должны наследоваться все репозитории,
+    использующие SQLAlchemy ORM.
+    """
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
