fa7b1e30c8633428ce91231c993cef7126d51c60
Agasyan <agasyan222@gmail.com>
2025-04-02 17:28:37 +0400

    Create a user end-points

--- backend/api/users.py
+++ backend/api/users.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-from core.schema.user import UserReadSchema, UserCreateSchema
-from .dependencies.auth.backend import auth_backend
-from .dependencies.auth.fastapi_users import fastapi_users
+from fastapi import APIRouter
+from fastapi.params import Depends
+
 from core.config import settings
+from core.schema.user import UserReadSchema, UserCreateSchema
+from core.service.user import UserService
+from .dependencies.auth.backend import auth_backend
+from .dependencies.auth.current_user import current_user
+from .dependencies.auth.fastapi_users import fastapi_users
+from .dependencies.services.user import get_user_service
 
 router = APIRouter(
     prefix=settings.api.auth,
@@ -23,3 +29,18 @@ router.include_router(
 router.include_router(
     router=fastapi_users.get_verify_router(UserReadSchema),
 )
+
+
+@router.get("/me", response_model=UserReadSchema)
+async def get_me(
+        user: Annotated[UserReadSchema, Depends(current_user)]
+):
+    return user
+
+
+@router.get("/{user_id}", response_model=UserReadSchema)
+async def get_user(
+        user_id: int,
+        service: Annotated[UserService, Depends(get_user_service)],
+):
+    return await service.get(user_id)
