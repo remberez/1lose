7c92f9b359bfe95e1c500d135a73183d7874e4c4
Agasyan <agasyan222@gmail.com>
2025-04-02 17:26:09 +0400

    add depends for service and repository for user model

---
+++ backend/api/dependencies/services/user.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models.db_helper import db_helper
+from core.repository.user import AbstractUserRepository, UserSQLAlchemyRepository
+from core.service.user import UserService
+
+
+async def get_sqlalchemy_user_repository(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+) -> UserSQLAlchemyRepository:
+    return UserSQLAlchemyRepository(session=session)
+
+
+async def get_user_service(
+        repository: Annotated[AbstractUserRepository, Depends(get_sqlalchemy_user_repository)],
+) -> UserService:
+    return UserService(repository=repository)
