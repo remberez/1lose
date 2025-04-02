e6fa3ca1352d5858502e1530dbe3e71d31991050
Agasyan <agasyan222@gmail.com>
2025-04-02 13:24:45 +0400

    create abstract repository for DB

---
+++ backend/core/repository/abc.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractRepository[ModelORM](ABC):
+    """
+    Интерфейс репозитория для работы с моделью.
+    Основное назначение инкапсулировать логику работы с данными.
+    """
+    @abstractmethod
+    async def get(self, **kwargs) -> ModelORM:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def create(self, **data) -> ModelORM | None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def update(self, model_id: int, **data) -> ModelORM | None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def delete(self, model_id: int) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def list(self, *args, **kwargs) -> list[ModelORM]:
+        raise NotImplementedError
