f6673f863a386e0149987f41e7fd0158f314c7fd
Agasyan <agasyan222@gmail.com>
2025-04-03 18:15:28 +0400

    add user_permission_handler

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,17 @@
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from starlette.requests import Request
 
 from api import router
-import uvicorn
 from core.config import settings
+from core.exceptions.user_exc import UserPermissionError
 from core.models.db_helper import db_helper
 
 
-# Lifespan приложения для освобождения пула соеденений к базе данных.
+# Lifespan приложения для освобождения пула соединений к базе данных.
 @asynccontextmanager
 async def lifespan(_app: FastAPI) -> AsyncGenerator[None, None]:
     yield
@@ -19,6 +20,13 @@ async def lifespan(_app: FastAPI) -> AsyncGenerator[None, None]:
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=router)
+
+
+@app.exception_handler(UserPermissionError)
+async def user_permission_handler(request: Request, exc: UserPermissionError):
+    # Обработчик для исключения UserPermissionError
+    raise HTTPException(status_code=403, detail="Insufficient access rights")
+
 
 # Входная точка
 if __name__ == "__main__":
