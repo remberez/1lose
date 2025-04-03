00b513ce2c459fbd32cd1128f7c32b1fd43de72e
Agasyan <agasyan222@gmail.com>
2025-04-03 18:10:43 +0400

    add permissions check

--- backend/api/games.py
+++ backend/api/games.py
@@ -1,11 +1,13 @@
 from typing import Annotated
+from typing import TYPE_CHECKING
 
 from fastapi import APIRouter
 from fastapi.params import Depends
 
 from core.config import settings
 from core.schema.game import GameCreateSchema, GameReadSchema
-from typing import TYPE_CHECKING
+from core.schema.user import UserReadSchema
+from .dependencies.auth.current_user import current_user
 from .dependencies.services.game import get_game_service
 
 if TYPE_CHECKING:
@@ -17,6 +19,7 @@ router = APIRouter(prefix=settings.api.games)
 @router.post("/", response_model=GameReadSchema)
 async def create_game(
     game: GameCreateSchema,
+    user: Annotated[UserReadSchema, Depends(current_user)],
     service: Annotated["GameService", Depends(get_game_service)],
 ):
-    return await service.create(game)
+    return await service.create(game=game, user_id=user.id)
--- backend/core/service/game.py
+++ backend/core/service/game.py
@@ -1,10 +1,17 @@
-from core.repository.game import GameRepository
+from core.const.user_role import UserRoleCodes
+from core.repository.game import AbstractRepository
+from core.repository.user import AbstractUserRepository
 from core.schema.game import GameCreateSchema, GameReadSchema
 
 
 class GameService:
-    def __init__(self, repository: GameRepository):
+    def __init__(self, repository: AbstractRepository, user_repository: AbstractUserRepository):
         self._repository = repository
+        self._user_repository = user_repository
 
-    async def create(self, game: GameCreateSchema) -> GameReadSchema:
-        return await self._repository.create(**game.model_dump())
+    async def create(self, game: GameCreateSchema, user_id: int) -> GameReadSchema:
+        # TODO: Добавить исключения
+        user = await self._user_repository.get(user_id)
+
+        if user.role_code == UserRoleCodes.ADMIN.value:
+            return await self._repository.create(**game.model_dump())
