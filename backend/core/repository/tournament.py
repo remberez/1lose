c21606f43d08416d800e4ada223235dc1085ee40
Agasyan <agasyan222@gmail.com>
2025-04-04 15:05:28 +0400

    create tournament repo

---
+++ backend/core/repository/tournament.py
@@ -0,0 +1,65 @@
+from abc import ABC
+from typing import Sequence
+
+from sqlalchemy import select, update, delete
+from typing_extensions import TypeVar
+
+from core.models import TournamentModel
+from core.repository.abc import AbstractReadRepository, AbstractWriteRepository
+from core.repository.sqlalchemy import SQLAlchemyAbstractRepository
+
+TournamentT = TypeVar("TournamentT")
+
+
+class AbstractTournamentRepository(
+    AbstractReadRepository,
+    AbstractWriteRepository,
+    ABC,
+):
+    # Специфичные методы для работы с TournamentModel
+    ...
+
+
+class SQLAlchemyTournamentRepository(
+    SQLAlchemyAbstractRepository[TournamentModel],
+    AbstractTournamentRepository,
+):
+    async def list(self) -> Sequence[TournamentModel]:
+        stmt = select(TournamentModel)
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
+
+    async def get(self, tournament_id: int) -> TournamentModel | None:
+        stmt = (
+            select(TournamentModel)
+            .where(TournamentModel.id == tournament_id)
+        )
+        result = await self._session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def create(self, **data) -> TournamentModel | None:
+        tournament = TournamentModel(**data)
+        self._session.add(tournament)
+        await self._session.commit()
+        await self._session.refresh(tournament)
+        return tournament
+
+    async def update(self, tournament_id: int, **data) -> TournamentModel:
+        stmt = (
+            update(TournamentModel)
+            .where(TournamentModel.id == tournament_id)
+            .values(**data)
+        )
+        await self._session.execute(stmt)
+        await self._session.commit()
+
+        tournament = await self.get(tournament_id)
+        return tournament
+
+    async def delete(self, tournament_id: int) -> None:
+        stmt = (
+            delete(TournamentModel)
+            .where(TournamentModel.id == tournament_id)
+        )
+        await self._session.execute(stmt)
+        await self._session.commit()
