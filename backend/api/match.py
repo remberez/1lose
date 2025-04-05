696ac8f6d3e11fa46f56aec57056d31ac5259a38
Agasyan <agasyan222@gmail.com>
2025-04-05 17:38:59 +0400

    create list route

--- backend/api/match.py
+++ backend/api/match.py
@@ -1,5 +1,18 @@
+from typing import Annotated
+
 from fastapi import APIRouter
+from fastapi.params import Depends
 
 from core.config import settings
+from core.schema.match import MatchReadSchema
+from core.service.match import MatchService
+from .dependencies.services.match import match_service
 
-router = APIRouter(prefix=settings.api.match)
+router = APIRouter(prefix=settings.api.match, tags=["Matches"])
+
+
+@router.get("/", response_model=list[MatchReadSchema])
+async def list_matches(
+        service: Annotated[MatchService, Depends(match_service)],
+):
+    return await service.list()
