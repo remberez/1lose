bdce4b35db355a03a8642f195dd7aeef1b8b59b5
Agasyan <agasyan222@gmail.com>
2025-04-05 15:49:07 +0400

    update method for tournament

--- backend/api/tournament.py
+++ backend/api/tournament.py
@@ -4,13 +4,13 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 
 from core.config import settings
-from core.schema.tournament import TournamentReadSchema, TournamentCreateSchema
+from core.schema.tournament import TournamentReadSchema, TournamentCreateSchema, TournamentUpdateSchema
 from core.schema.user import UserReadSchema
 from core.service.tournament import TournamentService
-from .dependencies.services.tournament import get_tournament_service
 from .dependencies.auth.current_user import current_user
+from .dependencies.services.tournament import get_tournament_service
 
-router = APIRouter(prefix=settings.api.tournament)
+router = APIRouter(prefix=settings.api.tournament, tags=["Tournaments"])
 
 
 @router.get("/{tournament_id}", response_model=TournamentReadSchema)
@@ -44,3 +44,17 @@ async def delete_tournament(
         service: Annotated[TournamentService, Depends(get_tournament_service)]
 ):
     await service.delete(user_id=user.id, tournament_id=tournament_id)
+
+
+@router.patch("/{tournament_id}", response_model=TournamentReadSchema)
+async def update_tournament(
+        tournament_id: int,
+        user: Annotated[UserReadSchema, Depends(current_user)],
+        service: Annotated[TournamentService, Depends(get_tournament_service)],
+        tournament: TournamentUpdateSchema,
+):
+    return await service.update(
+        tournament_id=tournament_id,
+        user_id=user.id,
+        **tournament.model_dump(exclude_none=True)
+    )
