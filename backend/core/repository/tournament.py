33ebc7cc803bff603d83ac27bb4afc0a18a7c581
Agasyan <agasyan222@gmail.com>
2025-04-05 19:23:02 +0400

    create tournament_exists function

--- backend/core/repository/tournament.py
+++ backend/core/repository/tournament.py
@@ -1,10 +1,11 @@
 from abc import ABC
 from typing import Sequence
 
-from sqlalchemy import select, update, delete
+from sqlalchemy import select, update, delete, exists
 from sqlalchemy.orm import joinedload
 from typing_extensions import TypeVar
 
+from core.exceptions.common import NotFoundError
 from core.models import TournamentModel
 from core.repository.abc import AbstractReadRepository, AbstractWriteRepository
 from core.repository.sqlalchemy import SQLAlchemyAbstractRepository
@@ -18,7 +19,8 @@ class AbstractTournamentRepository(
     ABC,
 ):
     # Специфичные методы для работы с TournamentModel
-    ...
+    async def tournament_exists(self, tournament_id: int) -> bool:
+        raise NotImplementedError()
 
 
 class SQLAlchemyTournamentRepository(
@@ -72,3 +74,9 @@ class SQLAlchemyTournamentRepository(
         )
         await self._session.execute(stmt)
         await self._session.commit()
+
+    async def tournament_exists(self, tournament_id: int) -> None:
+        # Вызывает NotFoundError, если турнир не был найден.
+        stmt = select(exists().where(TournamentModel.id == tournament_id))
+        if not bool(await self._session.scalar(stmt)):
+            raise NotFoundError(f"Tournament {tournament_id} not found")
