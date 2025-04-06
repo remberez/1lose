e7c7728c2182195124aff11c0e9f7d9bae4d183a
Agasyan <agasyan222@gmail.com>
2025-04-06 21:57:32 +0400

    add joinloaded strategy

--- backend/core/repository/match.py
+++ backend/core/repository/match.py
@@ -1,6 +1,11 @@
 from abc import ABC
+from typing import Sequence
+
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 from typing_extensions import TypeVar
 
+from core.models import TournamentModel, EATeamModel
 from core.repository.abc import AbstractReadRepository, AbstractWriteRepository
 from core.repository.sqlalchemy import SQLAlchemyAbstractWriteRepository, SQLAlchemyAbstractReadRepository
 from core.models.match import MatchModel
@@ -22,4 +27,27 @@ class SQLAlchemyMatchRepository(
     SQLAlchemyAbstractReadRepository[MatchModel],
     AbstractMatchRepository,
 ):
-    ...
+    async def get(self, model_id: int) -> MatchModel | None:
+        stmt = (
+            select(MatchModel)
+            .where(MatchModel.id == model_id)
+            .options(joinedload(
+                MatchModel.first_team,
+                MatchModel.second_team,
+                MatchModel.tournament
+            ))
+        )
+        result = await self._session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def list(self, *args, **kwargs) -> Sequence[MatchModel]:
+        stmt = (
+            select(MatchModel)
+            .options(
+                joinedload(MatchModel.first_team).joinedload(EATeamModel.game),
+                joinedload(MatchModel.second_team).joinedload(EATeamModel.game),
+                joinedload(MatchModel.tournament).joinedload(TournamentModel.game),
+            )
+        )
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
