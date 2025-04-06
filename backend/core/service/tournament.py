df7228e2f6b9b94e5a001b3299e7118384620d36
Agasyan <agasyan222@gmail.com>
2025-04-06 18:23:02 +0400

    add is_exists method in tournament service

--- backend/core/service/tournament.py
+++ backend/core/service/tournament.py
@@ -1,5 +1,6 @@
 from core.repository.tournament import AbstractTournamentRepository
 from .user import UserPermissionsService
+from ..exceptions.common import NotFoundError
 
 
 class TournamentService:
@@ -12,20 +13,27 @@ class TournamentService:
         self._repository = repository
         self._permissions_service = permissions_service
 
+    async def is_exists(self, tournament_id: int):
+        if not await self._repository.is_exists(tournament_id):
+            raise NotFoundError(f"Tournament {tournament_id} not found")
+
     async def create(self, user_id: int, **tournament_data):
         await self._permissions_service.verify_admin_or_moderator(user_id)
         return await self._repository.create(**tournament_data)
 
     async def update(self, user_id: int, tournament_id: int, **tournament_data):
         await self._permissions_service.verify_admin_or_moderator(user_id)
+        await self.is_exists(tournament_id)
         return await self._repository.update(tournament_id, **tournament_data)
 
     async def delete(self, user_id: int, tournament_id: int):
         await self._permissions_service.verify_admin_or_moderator(user_id)
+        await self.is_exists(tournament_id)
         return await self._repository.delete(tournament_id)
 
     async def list(self):
         return await self._repository.list()
 
     async def get(self, tournament_id):
+        await self.is_exists(tournament_id)
         return await self._repository.get(tournament_id)
