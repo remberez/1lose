e641b8706af055bae701d2eb234ac339d0fa5529
Agasyan <agasyan222@gmail.com>
2025-04-06 18:12:33 +0400

    add is_exists method in game repo

--- backend/core/service/game.py
+++ backend/core/service/game.py
@@ -1,3 +1,4 @@
+from core.exceptions.common import NotFoundError
 from core.repository.game import AbstractGameRepository
 from core.repository.user import AbstractUserRepository
 from core.schema.game import GameCreateSchema, GameReadSchema
@@ -15,6 +16,10 @@ class GameService:
         self._user_repository = user_repository
         self._permissions_service = permissions_service
 
+    async def is_exists(self, game_id: int):
+        if not await self._repository.is_exists(game_id):
+            raise NotFoundError(f"Game {game_id} not found")
+
     async def create(self, game: GameCreateSchema, user_id: int) -> GameReadSchema:
         await self._permissions_service.verify_admin(user_id=user_id)
         return await self._repository.create(**game.model_dump())
@@ -24,11 +29,14 @@ class GameService:
 
     async def delete(self, game_id: int, user_id: int) -> None:
         await self._permissions_service.verify_admin(user_id)
+        await self.is_exists(game_id)
         await self._repository.delete(game_id)
 
     async def get(self, game_id: int):
+        await self.is_exists(game_id)
         return await self._repository.get(game_id)
 
     async def update(self, game_id: int, user_id: int, **game_data):
         await self._permissions_service.verify_admin(user_id)
+        await self.is_exists(game_id)
         return await self._repository.update(game_id, **game_data)
