1ba239ab2a191725c0ddfb70d366789d19061c53
Agasyan <agasyan222@gmail.com>
2025-04-06 18:22:55 +0400

    add is_exists method in user service

--- backend/core/service/user.py
+++ backend/core/service/user.py
@@ -4,6 +4,7 @@ from typing import Callable, TypeVar, Any, Coroutine
 from typing_extensions import ParamSpec
 
 from core.const.user_role import UserRoleCodes
+from core.exceptions.common import NotFoundError
 from core.exceptions.user_exc import UserPermissionError
 from core.repository.user import AbstractUserRepository
 
@@ -16,7 +17,12 @@ class UserService:
     def __init__(self, repository: AbstractUserRepository):
         self._repository = repository
 
+    async def is_exists(self, user_id: int):
+        if not await self._repository.is_exists(user_id):
+            raise NotFoundError(f"User {user_id} not found")
+
     async def get(self, user_id: int) -> UserModelT:
+        await self.is_exists(user_id)
         return await self._repository.get(user_id)
 
 
