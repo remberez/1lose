9c15c9287a33b54ddfd55a0a8784c934f4978939
Agasyan <agasyan222@gmail.com>
2025-04-06 19:41:29 +0400

    change validation methods

--- backend/core/schema/match.py
+++ backend/core/schema/match.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
 from pydantic import BaseModel, Field, model_validator, field_validator
-
+from pydantic_core import PydanticCustomError
 
 class MatchSchema(BaseModel):
     tournament_id: int = Field(gt=0)
     first_team_id: int = Field(gt=0)
     second_team_id: int = Field(gt=0)
-    score: list[int] = Field(min_length=2, max_length=2)
+    score: list[int] = Field(min_length=0, max_length=2)
     best_of: int = Field(gt=0)
     date_start: datetime | None = None
     date_end: datetime | None = None
@@ -15,20 +15,29 @@ class MatchSchema(BaseModel):
     @model_validator(mode="after")
     def validate_dates(self):
         if self.date_end and self.date_start and self.date_end < self.date_start:
-            raise ValueError("The end date cannot be earlier than the start date")
+            raise PydanticCustomError(
+                "date_error",
+                "the end date cannot be earlier than the start date",
+            )
         return self
 
     @model_validator(mode="after")
     def validate_team_not_equals(self):
-        if self.first_team_id == self.second_team_id:
-            raise ValueError("The commands cannot be the same")
+        if self.first_team_id == self.second_team_id and (self.first_team_id or self.second_team_id):
+            raise PydanticCustomError(
+                "team_equal_error",
+                "the commands cannot be the same",
+            )
         return self
 
+    @field_validator("score", mode="before")
     @classmethod
-    @field_validator("score")
     def validate_score_items(cls, score: list[int]):
-        if any(x < 0 for x in score):
-            raise ValueError("All account values must be greater than or equal to zero")
+        if score and any(x < 0 for x in score):
+            raise PydanticCustomError(
+                "the_score_error",
+                "all account values must be greater than or equal to zero",
+            )
         return score
 
 class MatchCreateSchema(MatchSchema):
@@ -43,5 +52,5 @@ class MatchUpdateSchema(MatchSchema):
     tournament_id: int | None = Field(None, gt=0)
     first_team_id: int | None  = Field(None, gt=0)
     second_team_id: int | None = Field(None, gt=0)
-    score: list[int] = Field([], min_length=2, max_length=2)
+    score: list[int] | None = Field(None, min_length=0, max_length=2)
     best_of: int | None = Field(None, gt=0)
