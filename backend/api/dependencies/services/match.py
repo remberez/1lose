68b70c0c122434c9ee94c6a05da0cf65b0f32f8c
Agasyan <agasyan222@gmail.com>
2025-04-06 15:38:28 +0400

    add team depends

--- backend/api/dependencies/services/match.py
+++ backend/api/dependencies/services/match.py
@@ -6,9 +6,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models.db_helper import db_helper
 from core.repository.match import SQLAlchemyMatchRepository
+from core.repository.team import SQLAlchemyEATeamRepository
 from core.service.match import MatchService
 from .tournament import get_sqlalchemy_tournament_repository
 from .user import get_user_permissions_service
+from .team import get_sqlalchemy_ea_team_repository
 
 if typing.TYPE_CHECKING:
     from core.service.user import UserPermissionsService
@@ -23,10 +25,12 @@ async def sqlalchemy_match_repo(
 async def match_service(
         match_repo: Annotated[SQLAlchemyMatchRepository, Depends(sqlalchemy_match_repo)],
         permissions_service: Annotated["UserPermissionsService", Depends(get_user_permissions_service)],
-        tournament_repo: Annotated["SQLAlchemyTournamentRepository", Depends(get_sqlalchemy_tournament_repository)]
+        tournament_repo: Annotated["SQLAlchemyTournamentRepository", Depends(get_sqlalchemy_tournament_repository)],
+        team_repo: Annotated["SQLAlchemyEATeamRepository", Depends(get_sqlalchemy_ea_team_repository)],
 ) -> MatchService:
     return MatchService(
         repository=match_repo,
         permissions_service=permissions_service,
         tournament_repository=tournament_repo,
+        team_repository=team_repo,
     )
