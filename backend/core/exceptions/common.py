9eae5a10de4f0f57cc7230034516f3edf164f5a8
Agasyan <agasyan222@gmail.com>
2025-04-07 20:17:32 +0400

    create common business validation exception

--- backend/core/exceptions/common.py
+++ backend/core/exceptions/common.py
@@ -2,3 +2,6 @@ class NotFoundError(Exception):
     def __init__(self, *messages: str):
         self.messages = messages
         super().__init__(str(messages))  # Для обратной совместимости
+
+
+class BusinessValidationError(Exception): ...
