9a8f192d2cbb95236f4502525ed8898d0a430fbf
Agasyan <agasyan222@gmail.com>
2025-04-07 23:27:41 +0400

    create update, delete, get methods

--- backend/api/event.py
+++ backend/api/event.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 
 from core.config import settings
-from core.schema.event import EventReadSchema, EventCreateSchema
+from core.schema.event import EventReadSchema, EventCreateSchema, EventUpdateSchema
 from core.schema.user import UserReadSchema
 from core.service.event import EventService
 from .dependencies.services.event import event_service
@@ -27,3 +27,30 @@ async def create_event(
         event: EventCreateSchema,
 ):
     return await service.create(user.id, event)
+
+
+@router.get("/{event_id}", response_model=EventReadSchema)
+async def get_event(
+        service: Annotated[EventService, Depends(event_service)],
+        event_id: int,
+):
+    return await service.get(event_id)
+
+
+@router.patch("/{event_id}", response_model=EventReadSchema)
+async def update_event(
+        service: Annotated[EventService, Depends(event_service)],
+        event_id: int,
+        user: Annotated[UserReadSchema, Depends(current_user)],
+        event: EventUpdateSchema,
+):
+    return await service.update(event_id, user.id, event)
+
+
+@router.delete("/{event_id}")
+async def delete_event(
+        service: Annotated[EventService, Depends(event_service)],
+        event_id: int,
+        user: Annotated[UserReadSchema, Depends(current_user)],
+):
+    return await service.delete(event_id, user.id)
