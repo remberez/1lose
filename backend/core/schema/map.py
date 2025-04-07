5a3cab8fd679ec800ac55a5cb41ada020310c79d
Agasyan <agasyan222@gmail.com>
2025-04-07 15:32:12 +0400

    change schema fields

--- backend/core/schema/map.py
+++ backend/core/schema/map.py
@@ -1,18 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class MapSchema(BaseModel):
     match_id: int
     score: list[int]
-    winner_id: int
 
 
 class MapReadSchema(MapSchema):
     id: int
+    winner_id: int | None = None
 
 
 class MapUpdateSchema(MapSchema):
-    ...
+    winner_id: int | None = None
+    match_id: int | None = None
 
 
 class MapCreateSchema(MapSchema):
