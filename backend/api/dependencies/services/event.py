7c08e1b4797c940a8a0592c85ee54ca6ce40449e
Agasyan <agasyan222@gmail.com>
2025-04-08 10:21:15 +0400

    add outcome repo in depends

--- backend/api/dependencies/services/event.py
+++ backend/api/dependencies/services/event.py
@@ -4,8 +4,10 @@ from typing_extensions import Annotated
 
 from core.models import EventModel
 from core.models.db_helper import db_helper
+from core.models.event import OutComeModel
 from core.repository.event import SQLAlchemyEventRepository, AbstractEventRepository
 from core.repository.map import AbstractMapRepository
+from core.repository.outcome import SQLAlchemyOutComeRepository, AbstractOutComeRepository
 from core.service.user import UserPermissionsService
 from .user import get_user_permissions_service
 from core.service.event import EventService
@@ -18,14 +20,21 @@ async def sqlalchemy_event_repo(
     return SQLAlchemyEventRepository(session=session, model=EventModel)
 
 
+async def sqlalchemy_outcome_repo(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+) -> SQLAlchemyOutComeRepository:
+    return SQLAlchemyOutComeRepository(session=session, model=OutComeModel)
+
+
 async def event_service(
         repo: Annotated[AbstractEventRepository, Depends(sqlalchemy_event_repo)],
         permissions_service: Annotated[UserPermissionsService, Depends(get_user_permissions_service)],
-        map_respo: Annotated[AbstractMapRepository, Depends(sqlalchemy_map_repo)]
+        map_repo: Annotated[AbstractMapRepository, Depends(sqlalchemy_map_repo)],
+        outcome_repo: Annotated[AbstractOutComeRepository, Depends(sqlalchemy_outcome_repo)]
 ) -> EventService:
     return EventService(
         repository=repo,
         permissions_service=permissions_service,
-        map_repo=map_respo
+        map_repo=map_repo,
+        outcome_repo=outcome_repo,
     )
-
