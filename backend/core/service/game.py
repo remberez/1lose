a47b18f5a724905abd7b91f5cd30605f10c3e42c
Agasyan <agasyan222@gmail.com>
2025-04-08 18:09:06 +0400

    change game service for work with uow

--- backend/core/service/game.py
+++ backend/core/service/game.py
@@ -1,42 +1,50 @@
+from typing import Callable
+
 from core.exceptions.common import NotFoundError
-from core.repository.game import AbstractGameRepository
-from core.repository.user import AbstractUserRepository
 from core.schema.game import GameCreateSchema, GameReadSchema
 from core.service.user import UserPermissionsService
+from core.uow.uow import UnitOfWork
 
 
 class GameService:
     def __init__(
-        self,
-        repository: AbstractGameRepository,
-        user_repository: AbstractUserRepository,
-        permissions_service: UserPermissionsService,
+            self,
+            uow_factory: Callable[[], UnitOfWork],
+            permissions_service: UserPermissionsService
     ):
-        self._repository = repository
-        self._user_repository = user_repository
         self._permissions_service = permissions_service
+        self._uow_factory = uow_factory
 
     async def is_exists(self, game_id: int):
-        if not await self._repository.is_exists(game_id):
-            raise NotFoundError(f"Game {game_id} not found")
+        async with self._uow_factory() as uow:
+            if not await uow.games.is_exists(game_id):
+                raise NotFoundError(f"Game {game_id} not found")
 
     async def create(self, game: GameCreateSchema, user_id: int) -> GameReadSchema:
-        await self._permissions_service.verify_admin(user_id=user_id)
-        return await self._repository.create(**game.model_dump())
+        async with self._uow_factory() as uow:
+            await self._permissions_service.verify_admin(user_id=user_id)
+            return await uow.games.create(**game.model_dump())
 
     async def list(self) -> list[GameReadSchema]:
-        return await self._repository.list()
+        async with self._uow_factory() as uow:
+            return await uow.games.list()
 
     async def delete(self, game_id: int, user_id: int) -> None:
         await self._permissions_service.verify_admin(user_id)
-        await self.is_exists(game_id)
-        await self._repository.delete(game_id)
+
+        async with self._uow_factory() as uow:
+            await self.is_exists(game_id)
+            await uow.games.delete(game_id)
 
     async def get(self, game_id: int):
         await self.is_exists(game_id)
-        return await self._repository.get(game_id)
+
+        async with self._uow_factory() as uow:
+            return await uow.games.get(game_id)
 
     async def update(self, game_id: int, user_id: int, **game_data):
         await self._permissions_service.verify_admin(user_id)
         await self.is_exists(game_id)
-        return await self._repository.update(game_id, **game_data)
+
+        async with self._uow_factory() as uow:
+            return await uow.games.update(game_id, **game_data)
