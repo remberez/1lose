1e519cbc9761e8dea71b91c03ef9ca01fe673350
Agasyan <agasyan222@gmail.com>
2025-04-08 15:13:58 +0400

    change bet schema

--- backend/core/schema/bet.py
+++ backend/core/schema/bet.py
@@ -7,12 +7,14 @@ from core.const.bet_status import BetStatus
 
 class BetSchema(BaseModel):
     event_id: int = Field(gt=0)
+    outcome_id: int = Field(gt=0)
     amount: Decimal = Field(max_digits=12, decimal_places=2)
 
 
 class BetReadSchema(BetSchema):
     id: int = Field(gt=0)
     event_id: int = Field(exclude=True)
+    outcome_id: int = Field(exclude=True)
     coefficient: Decimal = Field(max_digits=12, decimal_places=2)
     possible_gain: Decimal = Field(max_digits=12, decimal_places=2)
     bet_status: BetStatus
