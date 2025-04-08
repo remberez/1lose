3d2762103f35f8b739ae8fd5c5a14514b66678d7
Agasyan <agasyan222@gmail.com>
2025-04-08 11:52:58 +0400

    add 'get_user_balance' and 'update_user_balance' funcs

--- backend/core/repository/user.py
+++ backend/core/repository/user.py
@@ -1,6 +1,9 @@
 from abc import ABC
+from decimal import Decimal
 
 from black.nodes import TypeVar
+from sqlalchemy import select, update
+
 from core.repository.abc import AbstractReadRepository
 from core.models.user import UserModel
 from .sqlalchemy import (
@@ -16,11 +19,29 @@ class AbstractUserRepository(
     ABC,
 ):
     # Специфичные методы для работы с пользователями.
-    ...
+    async def get_user_balance(self, user_id: int) -> Decimal:
+        raise NotImplementedError()
+
+    async def update_user_balance(self, user_id: int, value: Decimal) -> Decimal:
+        raise NotImplementedError()
 
 
 class UserSQLAlchemyRepository(
     SQLAlchemyAbstractReadRepository[UserModel],
     SQLAlchemyAbstractWriteRepository[UserModel],
     AbstractUserRepository,
-): ...
+):
+    async def get_user_balance(self, user_id: int) -> Decimal:
+        stmt = (
+            select(UserModel.balance)
+            .where(UserModel.id == user_id)
+        )
+        result = await self._session.execute(stmt)
+        return result.scalar_one()
+
+    async def update_user_balance(self, user_id: int, value: Decimal) -> Decimal:
+        user = await self.get(user_id)
+        user.balance += value
+        await self._session.commit()
+        return user.balance
+
