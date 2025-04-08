178b20f3a1c89272de56003ce95669a0f576657e
Agasyan <agasyan222@gmail.com>
2025-04-08 18:11:05 +0400

    change map service for work with uow

--- backend/core/service/map.py
+++ backend/core/service/map.py
@@ -1,31 +1,42 @@
-from core.repository.map import AbstractMapRepository
+from typing import Callable
+
 from core.schema.map import MapUpdateSchema, MapCreateSchema
 from core.service.user import UserPermissionsService
+from core.uow.uow import UnitOfWork
 
 
 class MapService:
     def __init__(
             self,
-            repository: AbstractMapRepository,
-            permissions_service: UserPermissionsService,
+            uow_factory: Callable[[], UnitOfWork],
+            permissions_service: UserPermissionsService
     ):
-        self._repo = repository
-        self._permission_service = permissions_service
+        self._permissions_service = permissions_service
+        self._uow_factory = uow_factory
 
     async def list(self):
-        return await self._repo.list()
+        async with self._uow_factory() as uow:
+            return await uow.maps.list()
 
     async def get(self, map_id: int):
-        return await self._repo.get(map_id)
+        async with self._uow_factory() as uow:
+            return await uow.maps.get(map_id)
 
     async def delete(self, map_id: int, user_id: int):
-        await self._permission_service.verify_admin(user_id)
-        await self._repo.delete(map_id)
+        # TODO: При удалении карты, вернуть деньги на баланс.
+        await self._permissions_service.verify_admin(user_id)
+
+        async with self._uow_factory() as uow:
+            await uow.maps.delete(map_id)
 
     async def update(self, map_id: int, user_id: int, map_data: MapUpdateSchema):
-        await self._permission_service.verify_admin_or_moderator(user_id)
-        return await self._repo.update(map_id, **map_data.model_dump(exclude_none=True))
+        await self._permissions_service.verify_admin_or_moderator(user_id)
+
+        async with self._uow_factory() as uow:
+            return await uow.maps.update(map_id, **map_data.model_dump(exclude_none=True))
 
     async def create(self, user_id: int, map_data: MapCreateSchema):
-        await self._permission_service.verify_admin_or_moderator(user_id)
-        return await self._repo.create(**map_data.model_dump())
+        await self._permissions_service.verify_admin_or_moderator(user_id)
+
+        async with self._uow_factory() as uow:
+            return await uow.maps.create(**map_data.model_dump())
