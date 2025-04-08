f984575de725110cc72d7e33eeb2996eb145d1d0
Agasyan <agasyan222@gmail.com>
2025-04-08 16:25:36 +0400

    change service for work with uow

--- backend/api/dependencies/services/bet.py
+++ backend/api/dependencies/services/bet.py
@@ -1,43 +1,17 @@
 from typing import Annotated
 
 from fastapi.params import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import BetModel
-from core.models.db_helper import db_helper
-from core.models.event import OutComeModel
-from core.repository.bet import SQLAlchemyBetRepository, AbstractBetRepository
-from core.repository.event import SQLAlchemyOutComeRepository, AbstractEventRepository, AbstractOutComeRepository
-from core.repository.user import AbstractUserRepository
 from core.service.bet import BetService
 from core.service.user import UserPermissionsService
-from .user import get_user_permissions_service, get_sqlalchemy_user_repository
-from .event import sqlalchemy_event_repo
-
-
-async def sqlalchemy_bet_repo(
-        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-) -> SQLAlchemyBetRepository:
-    return SQLAlchemyBetRepository(session=session, model=BetModel)
-
-
-async def sqlalchemy_outcome_repo(
-        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
-) -> SQLAlchemyOutComeRepository:
-    return SQLAlchemyOutComeRepository(session=session, model=OutComeModel)
+from core.uow.uow import UnitOfWork
+from .uow import get_uow
+from .user import get_user_permissions_service
 
 
 async def bet_service(
-        repo: Annotated[AbstractBetRepository, Depends(sqlalchemy_bet_repo)],
-        permissions_service: Annotated[UserPermissionsService, Depends(get_user_permissions_service)],
-        user_repo: Annotated[AbstractUserRepository, Depends(get_sqlalchemy_user_repository)],
-        event_repo: Annotated[AbstractEventRepository, Depends(sqlalchemy_event_repo)],
-        outcome_repo: Annotated[AbstractOutComeRepository, Depends(sqlalchemy_outcome_repo)],
+        uow_factory: Annotated[UnitOfWork, Depends(get_uow)],
+        permission_service: Annotated[UserPermissionsService, Depends(get_user_permissions_service)]
 ) -> BetService:
-    return BetService(
-        repo=repo,
-        permissions_service=permissions_service,
-        user_repository=user_repo,
-        event_repository=event_repo,
-        outcome_repository=outcome_repo,
-    )
+    return BetService(uow_factory=lambda: uow_factory, permissions_service=permission_service)
+
