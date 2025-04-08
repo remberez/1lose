0ba8981ea88c57d5bae66555c02a22a89921f4dc
Agasyan <agasyan222@gmail.com>
2025-04-08 15:13:46 +0400

    change create logic

--- backend/core/service/bet.py
+++ backend/core/service/bet.py
@@ -1,5 +1,7 @@
+from core.const.bet_status import BetStatus
 from core.exceptions.user_exc import UserPermissionError
 from core.repository.bet import AbstractBetRepository
+from core.repository.event import AbstractEventRepository, AbstractOutComeRepository
 from core.repository.user import AbstractUserRepository
 from core.schema.bet import BetCreateSchema, BetUpdateSchema
 from core.service.user import UserPermissionsService
@@ -12,10 +14,14 @@ class BetService:
             repo: AbstractBetRepository,
             permissions_service: UserPermissionsService,
             user_repository: AbstractUserRepository,
+            event_repository: AbstractEventRepository,
+            outcome_repository: AbstractOutComeRepository,
     ):
         self._repo = repo
         self._permissions_service = permissions_service
         self._user_repo = user_repository
+        self._event_repo = event_repository
+        self._outcome_repo = outcome_repository
 
     async def list(self):
         # TODO: Использовать только для модераторов/администраторов
@@ -43,8 +49,20 @@ class BetService:
         if user_balance < bet.amount:
             raise BusinessValidationError("Insufficient funds on the balance sheet")
 
+        outcome = await self._outcome_repo.get(bet.outcome_id)
+        event = await self._event_repo.get(bet.event_id)
+        if outcome.id not in (event.first_outcome_id, event.second_outcome_id):
+            raise BusinessValidationError("Outcome IDs do not match the event outcome IDs")
+
         await self._user_repo.update_user_balance(user_id, -bet.amount)
-        return await self._repo.create(**bet.model_dump(), user_id=user_id)
+
+        return await self._repo.create(
+            **bet.model_dump(),
+            user_id=user_id,
+            coefficient=outcome.coefficient,
+            bet_status=BetStatus.ACTIVE,
+            possible_gain=bet.amount * outcome.coefficient,
+        )
 
     async def update(self, bet_id: int, bet: BetUpdateSchema):
         ...
