74584bb40fada9f35ff94ef8c37bda6909aaae63
Agasyan <agasyan222@gmail.com>
2025-04-08 16:24:44 +0400

    change annotations

--- backend/core/uow/sqlalchemy.py
+++ backend/core/uow/sqlalchemy.py
@@ -1,11 +1,11 @@
 from core.models import BetModel, EventModel, GameModel, MapModel, MatchModel, EATeamModel, TournamentModel, UserModel
 from core.models.event import OutComeModel
 from core.repository.bet import AbstractBetRepository, SQLAlchemyBetRepository
-from core.repository.event import AbstractEventRepository, SQLAlchemyEventRepository, SQLAlchemyOutComeRepository
+from core.repository.event import AbstractEventRepository, SQLAlchemyEventRepository
 from core.repository.game import AbstractGameRepository, SQLAlchemyGameRepository
 from core.repository.map import AbstractMapRepository, SQLAlchemyMapRepository
 from core.repository.match import AbstractMatchRepository, SQLAlchemyMatchRepository
-from core.repository.outcome import AbstractOutComeRepository
+from core.repository.outcome import AbstractOutComeRepository, SQLAlchemyOutComeRepository
 from core.repository.team import AbstractEATeamRepository, SQLAlchemyEATeamRepository
 from core.repository.tournament import AbstractTournamentRepository, SQLAlchemyTournamentRepository
 from core.repository.user import AbstractUserRepository, UserSQLAlchemyRepository
@@ -17,15 +17,15 @@ class SqlAlchemyUnitOfWork(UnitOfWork):
         self.session_factory = session_factory
         self.session = None
 
-        self.bets: AbstractBetRepository | None
-        self.events: AbstractEventRepository | None
-        self.games: AbstractGameRepository | None
-        self.maps: AbstractMapRepository | None
-        self.matches: AbstractMatchRepository | None
-        self.outcomes: AbstractOutComeRepository | None
-        self.teams: AbstractEATeamRepository | None
-        self.tournaments: AbstractTournamentRepository | None
-        self.users: AbstractUserRepository | None
+        self.bets: AbstractBetRepository | None = None
+        self.events: AbstractEventRepository | None = None
+        self.games: AbstractGameRepository | None = None
+        self.maps: AbstractMapRepository | None = None
+        self.matches: AbstractMatchRepository | None = None
+        self.outcomes: AbstractOutComeRepository | None = None
+        self.teams: AbstractEATeamRepository | None = None
+        self.tournaments: AbstractTournamentRepository | None = None
+        self.users: AbstractUserRepository | None = None
 
     async def __aenter__(self):
         self.session = self.session_factory()
@@ -44,6 +44,8 @@ class SqlAlchemyUnitOfWork(UnitOfWork):
     async def __aexit__(self, exc_type, *_):
         if exc_type:
             await self.rollback()
+        else:
+            await self.commit()
         await self.session.close()
 
     async def commit(self):
