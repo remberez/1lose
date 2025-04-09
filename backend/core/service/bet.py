a619910d011c789385534e05b8c5365eeed53952
Agasyan <agasyan222@gmail.com>
2025-04-09 18:25:15 +0400

    refactoring

--- backend/core/service/bet.py
+++ backend/core/service/bet.py
@@ -1,10 +1,10 @@
+from decimal import Decimal
 from typing import Callable
 
 from core.const.bet_status import BetStatus
-from core.exceptions.user_exc import UserPermissionError
 from core.schema.bet import BetCreateSchema, BetUpdateSchema
 from core.service.user import UserPermissionsService
-from core.exceptions.common import BusinessValidationError
+from core.exceptions.common import BusinessValidationError, NotFoundError
 from core.uow.uow import UnitOfWork
 
 
@@ -34,11 +34,12 @@ class BetService:
         #   При добавлении тех. поддержки разрешить модераторам просматривать ставки,
         #   если активна сессия поддержки
         async with self._uow_factory() as uow:
+            await self._is_exists(bet_id, uow)
             bet = await uow.bets.get(bet_id)
 
-            if bet.user_id == user_id:
-                return bet
-            raise UserPermissionError()
+            if bet.user_id != user_id:
+                await self._permissions_service.verify_admin(user_id)
+            return bet
 
     async def delete(self, bet_id: int, user_id: int):
         async with self._uow_factory() as uow:
@@ -47,20 +48,24 @@ class BetService:
                 await self._permissions_service.verify_admin(user_id)
             await uow.bets.delete(bet_id)
 
+    async def _validate_user_balance(self, user_id: int, bet_amount: Decimal, uow: UnitOfWork):
+        user_balance = await uow.users.get_user_balance(user_id)
+        if user_balance < bet_amount:
+            raise BusinessValidationError("Insufficient funds on the balance sheet")
+
+    async def _validate_outcome_event(self, outcome_id: int, event_id: int, uow: UnitOfWork):
+        outcome = await uow.outcomes.get(outcome_id)
+        event = await uow.events.get(event_id)
+        if outcome.id not in (event.first_outcome_id, event.second_outcome_id):
+            raise BusinessValidationError("Outcome IDs do not match the event outcome IDs")
+
     async def create(self, user_id: int, bet: BetCreateSchema):
         async with self._uow_factory() as uow:
-            user_balance = await uow.users.get_user_balance(user_id)
-
-            if user_balance < bet.amount:
-                raise BusinessValidationError("Insufficient funds on the balance sheet")
-
-            outcome = await uow.outcomes.get(bet.outcome_id)
-            event = await uow.events.get(bet.event_id)
-            if outcome.id not in (event.first_outcome_id, event.second_outcome_id):
-                raise BusinessValidationError("Outcome IDs do not match the event outcome IDs")
-
+            await self._validate_user_balance(user_id, bet.amount, uow)
+            await self._validate_outcome_event(bet.outcome_id, bet.event_id, uow)
             await uow.users.update_user_balance(user_id, -bet.amount)
 
+            outcome = await uow.outcomes.get(bet.outcome_id)
             return await uow.bets.create(
                 **bet.model_dump(),
                 user_id=user_id,
