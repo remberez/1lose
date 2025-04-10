d7a48adb19a439e39841d749fe102960612a0c14
Agasyan <agasyan222@gmail.com>
2025-04-10 20:40:09 +0400

    override methods in business settings repository

--- backend/core/repository/business_settings.py
+++ backend/core/repository/business_settings.py
@@ -1,6 +1,6 @@
 from abc import ABC
 
-from sqlalchemy import select
+from sqlalchemy import select, delete, update
 
 from core.models import BusinessSettings
 from core.repository.abc import AbstractReadRepository, AbstractWriteRepository
@@ -28,3 +28,19 @@ class SQLAlchemyBusinessSettingsRepository(
         )
         result = await self._session.execute(stmt)
         return result.scalar_one().value
+
+    async def delete(self, settings_name: str) -> None:
+        stmt = (
+            delete(BusinessSettings)
+            .where(BusinessSettings.name == settings_name)
+        )
+        await self._session.execute(stmt)
+
+    async def update(self, settings_name: str, **data) -> BusinessSettings:
+        stmt = (
+            update(BusinessSettings)
+            .where(BusinessSettings.name == settings_name)
+            .values(**data)
+        )
+        result = await self._session.execute(stmt)
+        return result.scalar_one()
