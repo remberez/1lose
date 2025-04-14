a33607a238d4643a3adf54a1770f82e6b5b8ceee
Agasyan <agasyan222@gmail.com>
2025-04-14 22:01:19 +0400

    add methods in business settings service

--- backend/core/service/business_settings.py
+++ backend/core/service/business_settings.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+from core.schema.business_settings import BusinessSettingsUpdateSchema
 from core.service.user import UserPermissionsService
 from core.uow.uow import UnitOfWork
 
@@ -13,19 +14,27 @@ class BusinessSettingsService:
         self._uow_factory = uow
         self._permissions_service = permissions_service
 
-    async def list(self):
+    async def list(self, user_id: int):
+        await self._permissions_service.verify_admin(user_id)
         async with self._uow_factory() as uow:
             return await uow.business_settings.list()
 
-    async def get(self, settings_name: str):
+    async def get(self, settings_name: str, user_id: int):
+        await self._permissions_service.verify_admin(user_id)
         async with self._uow_factory() as uow:
             return await uow.business_settings.get(settings_name)
 
-    async def create(self, **data):
-        ...
+    async def create(self, user_id):
+        await self._permissions_service.verify_admin(user_id)
+        async with self._uow_factory() as uow:
+            return await uow.business_settings.create()
 
-    async def delete(self, settings_name: str):
-        ...
+    async def delete(self, settings_name: str, user_id: int):
+        await self._permissions_service.verify_admin(user_id)
+        async with self._uow_factory() as uow:
+            return await uow.business_settings.delete(settings_name)
 
-    async def update(self, settings_name: str):
-        ...
+    async def update(self, settings_name: str, settings_data: BusinessSettingsUpdateSchema, user_id: int):
+        await self._permissions_service.verify_admin(user_id)
+        async with self._uow_factory() as uow:
+            return await uow.business_settings.update(settings_name, **settings_data.model_dump())
