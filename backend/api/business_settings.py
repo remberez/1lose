758455b9df305928652cfe1dd01ed47045854870
Agasyan <agasyan222@gmail.com>
2025-04-14 22:41:37 +0400

    add create action for settings router

--- backend/api/business_settings.py
+++ backend/api/business_settings.py
@@ -4,6 +4,11 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 
 from core.config import settings
+from core.schema.business_settings import (
+    BusinessSettingsReadSchema,
+    BusinessSettingsUpdateSchema,
+    BusinessSettingsCreateSchema
+)
 from core.schema.user import UserReadSchema
 from core.service.business_settings import BusinessSettingsService
 from .dependencies.auth.current_user import current_user
@@ -12,10 +17,19 @@ from .dependencies.services.business_settings import business_settings
 router = APIRouter(prefix=settings.api.business, tags=["Business settings"])
 
 
-@router.get("/{settings_name}")
+@router.get("/{settings_name}", response_model=BusinessSettingsReadSchema)
 async def get_settings(
         settings_name: str,
         user: Annotated[UserReadSchema, Depends(current_user)],
         service: Annotated[BusinessSettingsService, Depends(business_settings)],
 ):
     return await service.get(settings_name, user.id)
+
+
+@router.post("/", response_model=BusinessSettingsReadSchema)
+async def create_settings(
+        settings_data: BusinessSettingsCreateSchema,
+        user: Annotated[UserReadSchema, Depends(current_user)],
+        service: Annotated[BusinessSettingsService, Depends(business_settings)],
+):
+    return await service.create(user.id, settings_data)
