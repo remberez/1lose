f511d09035ff38e30df168f3a52e24c6ca28958a
Agasyan <agasyan222@gmail.com>
2025-04-15 14:35:02 +0400

    add exists checking

--- backend/core/service/business_settings.py
+++ backend/core/service/business_settings.py
@@ -43,4 +43,5 @@ class BusinessSettingsService:
     async def update(self, settings_name: str, settings_data: BusinessSettingsUpdateSchema, user_id: int):
         await self._permissions_service.verify_admin(user_id)
         async with self._uow_factory() as uow:
-            return await uow.business_settings.update(settings_name, **settings_data.model_dump())
+            await self._is_exists(settings_name, uow)
+            return await uow.business_settings.update(settings_name, **settings_data.model_dump(exclude_none=True))
