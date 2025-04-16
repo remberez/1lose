34f8c0563beb9829dc052e6f426f72ac0a392576
Agasyan <agasyan222@gmail.com>
2025-04-16 20:20:23 +0400

    add user data header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { useEffect } from "react";
+import { userStore } from "../stores/authStore";
+import userService from "../services/userService";
+import {observer} from "mobx-react-lite"
+import authService from "../services/AuthService";
 
 const NavBarItem = ({ children }) => {
     return (
@@ -47,7 +52,25 @@ const NavBar = () => {
 }
 
 
-const Header = () => {
+const Header = observer(() => {
+    useEffect(() => {
+        const initAuth = async () => {
+            const token = localStorage.getItem("token");
+    
+            if (token) {
+                try {
+                    const userData = await userService.getMe();
+                    userStore.login(userData);
+                } catch (e) {
+                    console.error("Ошибка при проверке токена:", e);
+                    userStore.logout();
+                }
+            }
+        };
+
+        initAuth();
+    }, []);
+
     return (
         <header className="bg-oneWinBlue text-white">
             <div className="flex container items-center justify-between">
@@ -63,17 +86,41 @@ const Header = () => {
                     </Link>
                     <NavBar/>
                 </div>
-                <div className="flex gap-x-4 h-full text-sm">
-                    <Link className="bg-oneWinBlue-400 px-4 rounded-sm hover:bg-oneWinBlue-300 duration-200" to={"/login"}>
-                        Войти
-                    </Link>
-                    <Link className="bg-oneWinBrandBlue px-4 rounded-sm hover:bg-oneWinBrandBlue-600 duration-200" to={"/registration"}>
-                        Регистрация
-                    </Link>
+                <div className="flex items-center gap-x-4 h-full text-sm">
+                    {userStore.isAuth ? (
+                        <div className="flex items-center gap-x-4">
+                            <span className="text-white">Привет, {userStore.user?.email}</span>
+                            <button 
+                                onClick={async () => {
+                                    await authService.logout();
+                                    userStore.logout();
+                                    localStorage.removeItem("token");
+                                }}
+                                className="text-sm bg-red-500 hover:bg-red-600 px-3 py-1 rounded-md transition"
+                            >
+                                Выйти
+                            </button>
+                        </div>
+                    ) : (
+                        <>
+                            <Link 
+                                className="bg-oneWinBlue-400 px-4 py-1 rounded-sm hover:bg-oneWinBlue-300 duration-200" 
+                                to={"/login"}
+                            >
+                                Войти
+                            </Link>
+                            <Link 
+                                className="bg-oneWinBrandBlue px-4 py-1 rounded-sm hover:bg-oneWinBrandBlue-600 duration-200" 
+                                to={"/registration"}
+                            >
+                                Регистрация
+                            </Link>
+                        </>
+                    )}
                 </div>
             </div>
         </header>
     )
-}
+})
 
 export default Header;
