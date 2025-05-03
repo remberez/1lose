92b4887764a1e9d4894a7f6d8c76ff09d4347181
Agasyan <agasyan222@gmail.com>
2025-05-03 06:28:53 +0400

    create admin layout

---
+++ frontend/src/components/AdminLayout.jsx
@@ -0,0 +1,62 @@
+import { NavLink } from "react-router-dom"
+
+
+const SideBar = () => {
+    const linkClassName = ({ isActive }) =>
+        `font-inter px-4 py-2 rounded-lg hover:bg-oneWinBlue-500 transition ${
+        isActive ? "bg-oneWinBlue-400 font-semibold" : ""
+    }`
+
+    return (
+        <aside className="w-64 bg-oneWinBlue-600 text-white p-6 rounded-2xl shadow-lg">
+            <h1 className="text-2xl font-bold font-inter mb-8">Админ-панель</h1>
+            <nav className="flex flex-col gap-4">
+                <NavLink
+                    to="/admin/users"
+                    className={linkClassName}
+                >
+                    Пользователи
+                </NavLink>
+                <NavLink
+                    to="/admin/business-settings"
+                    className={linkClassName}
+                >
+                    Бизнес-настройки
+                </NavLink>
+                <NavLink
+                    to={"/admin/bets"}
+                    className={linkClassName}
+                >
+                    Ставки
+                </NavLink>
+                <NavLink
+                    to="/admin/matches"
+                    className={linkClassName}
+                >
+                    Матчи
+                </NavLink>
+                <NavLink
+                    to="/admin/matches"
+                    className={linkClassName}
+                >
+                    События
+                </NavLink>
+            </nav>
+        </aside>
+    );
+};
+
+const AdminLayout = () => {
+  return (
+    <div className="bg-oneWinBlue-300 min-h-screen">
+      <div className="container mx-auto flex pt-12 px-4">
+        <SideBar />
+        <div className="flex-grow ml-6 bg-white rounded-2xl p-6 shadow-md">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
