985c9bb09d0f0fdbccc7a06d9280e6e7b107b317
Agasyan <agasyan222@gmail.com>
2025-05-16 14:36:27 +0400

    add game update page

--- frontend/src/components/GameAdminList.jsx
+++ frontend/src/components/GameAdminList.jsx
@@ -1,42 +1,32 @@
-import { useEffect, useState } from "react";
 import AdminList from "./AdminList";
 import gameService from "../services/gameService";
+import { Link } from "react-router-dom"
 
-const columns = [
-    { key: "id", title: "ID" },
-    { key: "name", title: "Название" },
-    { key: "description", title: "Описание" },
-    {
-      key: "icon_path",
-      title: "Иконка",
-      render: (value) => (
-        <img src={value} alt="Иконка" className="w-8 h-8 object-contain" />
-      )
-    },
-    {
-      key: "actions",
-      title: "Действия",
-      render: (_, row) => (
-        <div className="flex gap-2">
-          <button className="text-blue-600 hover:underline">✏️</button>
-          <button className="text-red-600 hover:underline">🗑</button>
-        </div>
-      ),
-    },
-];
+
+const GameAdminList = ({games, onGameDelete}) => {
+    const columns = [
+      { key: "id", title: "ID" },
+      { key: "name", title: "Название" },
+      { key: "description", title: "Описание" },
+      {
+        key: "icon_path",
+        title: "Иконка",
+        render: (value) => (
+          <img src={value} alt="Иконка" className="w-8 h-8 object-contain" />
+        )
+      },
+      {
+        key: "actions",
+        title: "Действия",
+        render: (_, row) => (
+          <div className="flex gap-2">
+            <Link className="text-blue-600 hover:underline" to={row.id}>✏️</Link>
+            <button className="text-red-600 hover:underline" onClick={() => onGameDelete(row.id)}>🗑</button>
+          </div>
+        ),
+      },
+    ];
   
-
-const GameAdminList = () => {
-    const [games, setGames] = useState([]);
-    
-    useEffect(() => {
-        async function fetchData() {
-            const gamesData = await gameService.getGames();
-            setGames(gamesData);
-        }
-
-        fetchData();
-    }, [])
     return (
         <AdminList data={games} columns={columns} />
     )
--- frontend/src/services/gameService.js
+++ frontend/src/services/gameService.js
@@ -9,6 +9,25 @@ class GameService {
             console.error(error);
         }
     }
+
+    async createGame(data) {
+        try {
+            const response = await api.post("/games/", data, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+            });
+            return response.data;
+        } catch (error) {
+            console.error("Ошибка при создании игры:", error);
+            throw error;
+        }
+    }
+
+    async deleteGame(gameId) {
+        const response = await api.delete(`/games/${gameId}`);
+        return response;
+    }
 }
 
-export default new GameService();
+    export default new GameService();
