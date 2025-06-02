6f3756086086d917c150edec27d2c3f925b2c0c6
Agasyan <agasyan222@gmail.com>
2025-06-02 20:34:47 +0400

    delete fastapi-users routers and create custom registration

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,26 +1,22 @@
+from typing import Annotated
 
 from fastapi import APIRouter
+from fastapi.params import Depends
 
+from api.dependencies.services.user import get_user_service
 from core.config import settings
-from core.schema.user import UserReadSchema, UserCreateSchema
-from .dependencies.auth.backend import auth_backend
-from .dependencies.auth.fastapi_users import fastapi_users
+from core.schema.user import UserReadSchema, UserRegisterSchema
+from core.service.user import UserService
 
 router = APIRouter(
     prefix=settings.api.auth,
     tags=["Auth"],
 )
 
-# login, logout
-router.include_router(
-    router=fastapi_users.get_auth_router(auth_backend),
-)
 
-# register
-router.include_router(
-    router=fastapi_users.get_register_router(UserReadSchema, UserCreateSchema),
-)
-
-router.include_router(
-    router=fastapi_users.get_verify_router(UserReadSchema),
-)
+@router.post("/register", response_model=UserReadSchema)
+async def register_user(
+        user_data: UserRegisterSchema,
+        user_service: Annotated[UserService, Depends(get_user_service)]
+):
+    return await user_service.register(user_data)
