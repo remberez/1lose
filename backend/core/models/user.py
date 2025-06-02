bcca1c4de68a485a6cda90e56dfd31b14327d834
Agasyan <agasyan222@gmail.com>
2025-06-02 18:13:57 +0400

    change user model for custom model

--- backend/core/models/user.py
+++ backend/core/models/user.py
@@ -1,27 +1,24 @@
 import typing
+from decimal import Decimal
 
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column,
-    relationship,
-)
 from sqlalchemy import (
     Numeric,
     CheckConstraint,
     String,
     ForeignKey,
-    text,
+    text, Boolean,
 )
-from decimal import Decimal
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
 
 from .base import (
     Base,
     IntegerIDMixin,
     DateCreatedUpdatedMixin,
 )
-from core.types.user_id import UserID
 
 if typing.TYPE_CHECKING:
     from core.models import EventModel
@@ -38,10 +35,15 @@ class UserRoleModel(Base):
 
 
 class UserModel(
-    Base, DateCreatedUpdatedMixin, IntegerIDMixin, SQLAlchemyBaseUserTable[UserID]
+    Base, DateCreatedUpdatedMixin, IntegerIDMixin
 ):
     __table_args__ = (CheckConstraint("balance >= 0", name="non_negative_balance"),)
+    __tablename__ = "user"
 
+    email: Mapped[str] = mapped_column(String(320), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     balance: Mapped[Decimal] = mapped_column(Numeric(12, 2), default=Decimal("0.00"))
     role_code: Mapped[str] = mapped_column(
         ForeignKey("user_role.code"), server_default=text("'user'")
@@ -50,8 +52,3 @@ class UserModel(
     role: Mapped["UserRoleModel"] = relationship(back_populates="users")
     events_updated: Mapped[list["EventModel"]] = relationship(back_populates="updated_by_r")
     bets: Mapped[list["BetModel"]] = relationship(back_populates="user")
-
-
-    @classmethod
-    def get_db(cls, session: AsyncSession):
-        return SQLAlchemyUserDatabase(session, cls)
