bf06dca01494b7456025695d0bc6a5a648f0442a
Agasyan <agasyan222@gmail.com>
2025-06-02 18:15:22 +0400

    create get_current_token_payload function

--- backend/api/dependencies/auth/current_user.py
+++ backend/api/dependencies/auth/current_user.py
@@ -1,7 +1,52 @@
-from .fastapi_users import fastapi_users
+from typing import Annotated
 
-# Зависимости для получения авторизированного пользователя
-current_user = fastapi_users.current_user()
-current_user_optional = fastapi_users.current_user(optional=True)
-current_active_user = fastapi_users.current_user(active=True)
-current_active_verify_user = fastapi_users.current_user(active=True, verified=True)
+from fastapi import HTTPException
+from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordBearer
+from jwt import InvalidTokenError
+
+from core.config import settings
+from core.utils.auth import decode_token
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl=settings.api.token_url)
+
+async def get_current_token_payload(
+    token: Annotated[str, Depends(oauth2_schema)],
+) -> dict:
+    # Принимает в себя токен в запросе и возвращает payload токена
+    invalid_token_exc = HTTPException(
+        status_code=401,
+        detail="Token invalid"
+    )
+    try:
+        payload = decode_token(token)
+    except InvalidTokenError:
+        raise invalid_token_exc
+    return payload
+
+async def get_current_user_from_token_type(token_type: str):
+    ...
+
+
+async def get_current_active_verify_user(
+
+):
+    ...
+
+
+async def get_current_user(
+
+):
+    ...
+
+
+async def get_active_user(
+
+):
+    ...
+
+
+async def get_verified_user(
+
+):
+    ...
