8151e8eeb3afcff7fbeee688c2b562a9d54a2487
Agasyan <agasyan222@gmail.com>
2025-06-07 20:44:17 +0400

    add event_type model and nullable event name

--- backend/core/models/event.py
+++ backend/core/models/event.py
@@ -12,6 +12,15 @@ if typing.TYPE_CHECKING:
     from core.models import BetModel
 
 
+class EventTypeModel(Base):
+    __tablename__ = "event_type"
+
+    code: Mapped[str] = mapped_column(String(32), primary_key=True)
+    name: Mapped[str] = mapped_column(String(64))
+
+    events: Mapped[list["EventModel"]] = relationship(back_populates="type")
+
+
 class EventOutcomeModel(Base):
     __tablename__ = "event_outcome"
 
@@ -22,7 +31,7 @@ class EventOutcomeModel(Base):
 class OutComeModel(Base, IntegerIDMixin):
     __tablename__ = "outcome"
 
-    name: Mapped[str] = mapped_column(String(32))
+    name: Mapped[str] = mapped_column(String(32), nullable=True)
     coefficient: Mapped[float] = mapped_column(Numeric(10, 2))
 
     bets: Mapped[list["BetModel"]] = relationship(back_populates="outcome")
@@ -34,6 +43,7 @@ class EventModel(Base, IntegerIDMixin, DateCreatedUpdatedMixin, UpdatedByMixin):
     match_id: Mapped[int] = mapped_column(ForeignKey("match.id", ondelete="CASCADE"))
     map_id: Mapped[int] = mapped_column(ForeignKey("map.id", ondelete="CASCADE"), nullable=True)
     name: Mapped[str] = mapped_column(String(32))
+    type_code: Mapped["EventTypeModel"] = mapped_column(ForeignKey("event_type.code", ondelete="CASCADE"))
 
     match: Mapped["MatchModel"] = relationship(back_populates="events", foreign_keys=match_id)
     map: Mapped["MapModel"] = relationship(back_populates="events")
@@ -43,3 +53,4 @@ class EventModel(Base, IntegerIDMixin, DateCreatedUpdatedMixin, UpdatedByMixin):
                                                             foreign_keys="MatchModel.win_event_id",
                                                             uselist=False)
     outcomes: Mapped[list["OutComeModel"]] = relationship(secondary=EventOutcomeModel.__table__)
+    type: Mapped["EventTypeModel"] = relationship(back_populates="events")
